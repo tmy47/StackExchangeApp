a8165fba401893a155fa39249ffa65aa5ef060db
timparmentier@gmail.com <timparmentier@gmail.com>
2020-09-25 13:20:44 -0400

    Initial Commit

---
+++ StackExchange/StackExchange/StackExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackExchangeApp.swift
+//  StackExchange
+//
+//  Created by Tim Parmentier on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct StackExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
